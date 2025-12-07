ade1913155a504467ef99ea04d34098f42f48508
Sakkie <127411898+prismaticpink@users.noreply.github.com>
2025-12-07 12:32:43 +0000

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".item").forEach(item => {
+  const img = item.querySelector(".open-popup");
+  const popup = item.querySelector(".popup");
+  const close = item.querySelector(".close");
+
+  img.onclick = () => popup.style.display = "flex";
+  close.onclick = () => popup.style.display = "none";
+
+  popup.onclick = e => {
+    if (e.target === popup) popup.style.display = "none";
+  };
+});
