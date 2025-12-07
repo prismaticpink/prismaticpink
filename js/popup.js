c6f0ca4b0f396f67cfec6d4ce157aeb8a2e93447
Sakkie <127411898+prismaticpink@users.noreply.github.com>
2025-12-07 12:58:29 +0000

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -3,10 +3,12 @@ document.querySelectorAll(".item").forEach(item => {
   const popup = item.querySelector(".popup");
   const close = item.querySelector(".close");
 
-  img.onclick = () => popup.style.display = "flex";
-  close.onclick = () => popup.style.display = "none";
+  if (popup && close) {
+    img.onclick = () => popup.style.display = "flex";
+    close.onclick = () => popup.style.display = "none";
 
-  popup.onclick = e => {
-    if (e.target === popup) popup.style.display = "none";
-  };
+    popup.onclick = e => {
+      if (e.target === popup) popup.style.display = "none";
+    };
+  }
 });
