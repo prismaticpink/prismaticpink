275476e34c518bb53c786f023231aa14aa1f84e9
Sakkie <127411898+prismaticpink@users.noreply.github.com>
2026-07-25 18:33:24 +0100

    Add files via upload

---
+++ js/description.js
@@ -0,0 +1,18 @@
+console.log("description.js loaded");
+
+document.querySelectorAll(".item").forEach(item => {
+    const description = item.querySelector(".description");
+
+    item.addEventListener("mouseenter", () => {
+        description.style.opacity = "1";
+    });
+
+    item.addEventListener("mouseleave", () => {
+        description.style.opacity = "0";
+    });
+
+    item.addEventListener("mousemove", (e) => {
+        description.style.left = `${e.clientX + 12}px`;
+        description.style.top = `${e.clientY + 12}px`;
+    });
+});
