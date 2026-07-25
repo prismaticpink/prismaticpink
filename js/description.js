cd0b93b23cca6256c9913c022799e8ad6cf11b4c
Sakkie <127411898+prismaticpink@users.noreply.github.com>
2026-07-26 00:03:22 +0100

    Update description.js

--- js/description.js
+++ js/description.js
@@ -16,3 +16,22 @@ document.querySelectorAll(".item").forEach(item => {
         description.style.top = `${e.clientY + 12}px`;
     });
 });
+
+console.log("description.js loaded");
+
+document.querySelectorAll(".item").forEach(item => {
+    const description = item.querySelector(".description-buttons");
+
+    item.addEventListener("mouseenter", () => {
+        description.style.opacity = "1";
+    });
+
+    item.addEventListener("mouseleave", () => {
+        description.style.opacity = "0";
+    });
+
+    item.addEventListener("mousemove", (e) => {
+        description.style.left = `${e.clientX + 12}px`;
+        description.style.top = `${e.clientY + 12}px`;
+    });
+});
